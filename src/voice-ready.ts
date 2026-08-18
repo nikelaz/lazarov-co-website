7decf80836ef0014b82aa560f26b204337493c90
Nikola Lazarov <nikola.n.lazarov@outlook.com>
2026-08-18 21:34:17 +0300

    feat: macos demo release

--- src/voice-ready.ts
+++ src/voice-ready.ts
@@ -1,2 +1,3 @@
 export const voiceReadyDemoStoreUrl = 'https://apps.microsoft.com/store/detail/9MV9J3QK4K09?cid=DevShareMCLPCS';
 export const voiceReadyStoreUrl = 'https://apps.microsoft.com/detail/9P1LF881W64N?hl=en-us&gl=US&ocid=pdpshare';
+export const voiceReadyMacDemoUrl = '/downloads/VoiceReadyDemo-v1.0.2.pkg';
